8ed2281b4d7b77b28715c9a10bb86177b956b138
N.S.Devaraj <nsdevaraj@gmail.com>
2016-04-26 12:13:41 +0530

    added files back

---
+++ HealthClient/src/client/app/vitals/services/vitals.form.client.service.js
@@ -0,0 +1,93 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.vital')
+        .factory('VitalForm', factory);
+
+    function factory() {
+
+        var getFormFields = function(disabled) {
+
+            var fields = [
+                {
+                    key: 'name',
+                    type: 'input',
+                    templateOptions: {
+                        label: 'Vital Alias:',
+                        disabled: disabled,
+                        required: true
+                    }
+                },
+                {
+                    key: 'interval',
+                    type: 'input',
+                    templateOptions: {
+                        label: 'Interval:',
+                        disabled: disabled,
+                        required: true
+                    }
+                },
+                {
+                    key: 'minsp02',
+                    type: 'input',
+                    templateOptions: {
+                        label: 'minsp02:',
+                        disabled: disabled
+                    }
+                },
+                {
+                    key: 'maxsp02',
+                    type: 'input',
+                    templateOptions: {
+                        label: 'maxsp02:',
+                        disabled: disabled
+                    }
+                },
+                {
+                    key: 'minbeat',
+                    type: 'input',
+                    templateOptions: {
+                        label: 'minbeat:',
+                        disabled: disabled
+                    }
+                },
+                {
+                    key: 'maxbeat',
+                    type: 'input',
+                    templateOptions: {
+                        label: 'maxbeat:',
+                        disabled: disabled
+                    }
+                },
+                {
+                    key: 'mintemp',
+                    type: 'input',
+                    templateOptions: {
+                        label: 'mintemp:',
+                        disabled: disabled
+                    }
+                },
+                {
+                    key: 'maxtemp',
+                    type: 'input',
+                    templateOptions: {
+                        label: 'maxtemp:',
+                        disabled: disabled
+                    }
+                }
+            ];
+
+            return fields;
+
+        };
+
+        var service = {
+            getFormFields: getFormFields
+        };
+
+        return service;
+
+    }
+
+})();
